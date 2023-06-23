be21dd5c60cc0cab046ad2ca9203cb3f2586bcd8
Octavio García Larrecharte <okigarcia06@gmail.com>
2023-06-23 17:41:45 -0300

    agregando link active en el menu y detalles

---
+++ script.js
@@ -0,0 +1,10 @@
+const navLinksElem = document.querySelectorAll('.nav-link');
+const windowsPathName = window.location.pathname;
+
+navLinksElem.forEach(navLinkEl => {
+    const navLinkPathName = new URL(navLinkEl.href).pathname;
+
+    if (windowsPathName === navLinkPathName || (windowsPathName === '/index.html' &&  navLinkPathName === '/')) {
+        navLinkEl.classList.add('active');
+    }
+})
