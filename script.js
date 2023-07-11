903b83bde3c7afa60330ec057a643e8400eccef3
Octavio García Larrecharte <okigarcia06@gmail.com>
2023-07-11 13:36:01 -0300

    Agregamos comentarios y emprolijamos el código

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+// Función para marcar sección Activa en Menú
+
 const navLinksElem = document.querySelectorAll('.nav-link');
 const windowsPathName = window.location.pathname;
 
+// Comparación
 navLinksElem.forEach(navLinkEl => {
     const navLinkPathName = new URL(navLinkEl.href).pathname;
 
+    // Agregamos clase active
     if (windowsPathName === navLinkPathName || (windowsPathName === './index.html' &&  navLinkPathName === '/')) {
         navLinkEl.classList.add('active');
     }
