e4524074157d17852ccf1e1d2097cf4846eef329
Octavio García Larrecharte <okigarcia06@gmail.com>
2023-09-02 21:46:24 -0300

    agregando fix btn y js

--- script.js
+++ script.js
@@ -32,24 +32,48 @@ navLinksElem.forEach(navLinkEl => {
 
 // Escuchar evento submit del form
 
-const $form = document.querySelector('#form')
-const $buttonMailTo = document.querySelector('#enviarMail')
+document.addEventListener('DOMContentLoaded', function() {
+    const $form = document.querySelector('#form');
+    const $buttonMailTo = document.querySelector('#enviarMail');
 
-$form.addEventListener('submit', handleSubmit)
+    $form.addEventListener('submit', handleSubmit);
 
-function handleSubmit(event) {
-    event.preventDefault()
-    const form = new FormData(this)
+    function handleSubmit(event) {
+        event.preventDefault()
+        const form = new FormData(this)
 
-    const nombre = form.get('nombre');
-    const email = form.get('email');
-    const mensaje = form.get('mensaje');
+        const nombre = form.get('nombre');
+        const email = form.get('email');
+        const mensaje = form.get('mensaje');
 
-    if (!nombre || !email || !mensaje) {
-        alert('Por favor, complete todos los campos antes de enviar el formulario.');
-        return;
-    }
+        if (!nombre || !email || !mensaje) {
+            alert('Por favor, complete todos los campos antes de enviar el formulario.');
+            return;
+        }
 
     $buttonMailTo.setAttribute('href', `mailto:g_grafica@hotmail.com?subject=${form.get('nombre')} - ${form.get('email')}&body=${form.get('mensaje')}`)
     $buttonMailTo.click()
 }
+});
+
+// const $form = document.querySelector('#form')
+// const $buttonMailTo = document.querySelector('#enviarMail')
+
+// $form.addEventListener('submit', handleSubmit)
+
+// function handleSubmit(event) {
+//     event.preventDefault()
+//     const form = new FormData(this)
+
+//     const nombre = form.get('nombre');
+//     const email = form.get('email');
+//     const mensaje = form.get('mensaje');
+
+//     if (!nombre || !email || !mensaje) {
+//         alert('Por favor, complete todos los campos antes de enviar el formulario.');
+//         return;
+//     }
+
+//     $buttonMailTo.setAttribute('href', `mailto:g_grafica@hotmail.com?subject=${form.get('nombre')} - ${form.get('email')}&body=${form.get('mensaje')}`)
+//     $buttonMailTo.click()
+// }
