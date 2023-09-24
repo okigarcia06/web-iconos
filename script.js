3c4b40984aaafbb4a6efa9f0564b23464b486114
Octavio García Larrecharte <okigarcia06@gmail.com>
2023-09-24 13:13:42 -0300

    Se agregan promesas y fetch

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-// validar form de contacto
+// Validar form de contacto
 (() => {
     'use strict'
     const forms = document.querySelectorAll('.needs-validation')
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-    }, false)
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+        }, false)
     })
-})()
-
+})();
 
 // Función para marcar sección Activa en Menú
-
 const navLinksElem = document.querySelectorAll('.nav-link');
 const windowsPathName = window.location.pathname;
 
@@ -29,32 +27,78 @@ navLinksElem.forEach(navLinkEl => {
     }
 })
 
+//ENVIO DE FORM
 
-// Escuchar evento submit del form
+// Se agrega un evento de clic al botón de envío del formulario
+const $enviarBtn = document.querySelector('#boton-enviar');
+$enviarBtn.addEventListener('click', enviarFormulario);
 
-document.addEventListener('DOMContentLoaded', function() {
-    const $form = document.querySelector('#form');
-    const $buttonMailTo = document.querySelector('#enviarMail');
+// Función para enviar el formulario con una solicitud Fetch simulada
+function enviarFormulario(event) {
+    event.preventDefault(); // Prevenir el envío del formulario por defecto
+    
+    const nombre = document.querySelector('#nombre').value;
+    const email = document.querySelector('#email').value;
+    const nroContacto = document.querySelector('#nroContacto').value;
+    const mensaje = document.querySelector('#mensaje').value;
 
-    $form.addEventListener('submit', handleSubmit);
+    // Verificar si los campos están completos
+    if (!nombre || !email || !nroContacto || !mensaje) {
+        mostrarAlertaError('Por favor, complete todos los campos antes de enviar el formulario.');
+        return;
+    }
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const form = new FormData(this)
+    // Verificar si el campo de correo electrónico y número de contacto tienen un formato válido
+    const esValido = validarEmailYContacto(email, nroContacto);
+    if (!esValido) {
+        mostrarAlertaError('Por favor, ingrese un correo electrónico y número de contacto válidos.');
+        return;
+    }
 
-        const nombre = form.get('nombre');
-        const email = form.get('email');
-        const mensaje = form.get('mensaje');
+    // Simulación de una respuesta (puede ser cualquier valor)
+    const respuestaSimulada = { mensaje: 'Formulario enviado con éxito' };
 
-        if (!nombre || !email || !mensaje) {
-            alert('Por favor, complete todos los campos antes de enviar el formulario.');
-            return;
-        }
-
-    $buttonMailTo.setAttribute('href', `mailto:g_grafica@hotmail.com?subject=${form.get('nombre')} - ${form.get('email')}&body=${form.get('mensaje')}`)
-    $buttonMailTo.click()
+    // Simular un retardo para emular una solicitud asíncrona
+    setTimeout(() => {
+        mostrarAlertaExito(respuestaSimulada.mensaje);
+        // Se limpian los datos del formulario una vez enviado.
+        document.querySelector('#nombre').value = '';
+        document.querySelector('#email').value = '';
+        document.querySelector('#nroContacto').value = '';
+        document.querySelector('#mensaje').value = '';
+    }, 1000); // Simular un retardo de 1 segundo
 }
-});
+
+// Función para mostrar una alerta de error con SweetAlert
+function mostrarAlertaError(mensaje) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: mensaje
+    });
+}
+
+// Función para mostrar una alerta de éxito con SweetAlert
+function mostrarAlertaExito(mensaje) {
+    Swal.fire({
+        icon: 'success',
+        title: 'Éxito',
+        text: mensaje
+    });
+}
+
+// Función para validar el formato de correo electrónico y número de contacto
+function validarEmailYContacto(email, nroContacto) {
+    const expresionEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const expresionContacto = /^[0-9\-+()\s]+$/; // Permite números, -, +, (), y espacios
+    
+    const esEmailValido = expresionEmail.test(email);
+    // Falta verificar la validez del número de contacto y retornar el resultado
+    const esContactoValido = expresionContacto.test(nroContacto);
+    
+    // Retorna true solo si tanto el correo como el número de contacto son válidos
+    return esEmailValido && esContactoValido;
+}
 
 //CARRITO DE COMPRAS DE PRODUCTOS
 
@@ -87,24 +131,33 @@ function addProduct(e){
 function deleteProduct(e) {
     if (e.target.classList.contains('delete-product')) {
         const deleteId = e.target.getAttribute('data-id');
+        
+        // Mostrar un mensaje de confirmación con SweetAlert
+        Swal.fire({
+            title: '¿Está seguro que desea eliminar este producto?',
+            text: 'Esta acción no se puede deshacer.',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Sí, eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                buyThings.forEach(value => {
+                    if (value.id == deleteId) {
+                        let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
+                        totalCard =  totalCard - priceReduce;
+                        totalCard = totalCard.toFixed(2);
+                    }
+                });
+                buyThings = buyThings.filter(product => product.id !== deleteId);
+                
+                countProduct--;
 
-        buyThings.forEach(value => {
-            if (value.id == deleteId) {
-                let priceReduce = parseFloat(value.price) * parseFloat(value.amount);
-                totalCard =  totalCard - priceReduce;
-                totalCard = totalCard.toFixed(2);
+                // Actualizar la interfaz después de eliminar el producto
+                loadHtml();
             }
         });
-        buyThings = buyThings.filter(product => product.id !== deleteId);
-        
-        countProduct--;
     }
-    //FIX: El contador se quedaba con "1" aunque ubiera 0 productos
-    if (buyThings.length === 0) {
-        priceTotal.innerHTML = 0;
-        amountProduct.innerHTML = 0;
-    }
-    loadHtml();
 }
 
 function readTheContent(product){
@@ -160,6 +213,7 @@ function loadHtml(){
 
         amountProduct.innerHTML = countProduct;
     });
+
     // Reinicia el contador de productos y el precio total
     priceTotal.innerHTML = totalCard;
     amountProduct.innerHTML = countProduct;
